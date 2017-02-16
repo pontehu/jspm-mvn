fc237eaf6447a8341145b89f92568ce24c9bb069
zenorbi <norbert.zentai@ponte.hu>
2017-02-16 13:14:31 +0100

    move to new repo

--- javascript/src/config.ts
+++ javascript/src/config.ts
@@ -2,6 +2,6 @@ export const groupId = "hu.ponte.js";
 export const packagePathInArtifact = "jspackage/";
 export const helperJarPath = "bin/helper-jar-with-dependencies.jar";
 export const jspmMvnRepositories:{[id:string]:string} = {
-	"ponte-maven3-repo-deploy-snapshot": "http://origo:8081/nexus/content/repositories/snapshots",
-	"ponte-maven3-repo-deploy-release": "http://origo:8081/nexus/content/repositories/releases"
+	"ponte-maven3-repo-deploy-snapshot": "http://nexus.ponte:8081/repository/snapshots",
+	"ponte-maven3-repo-deploy-release": "http://nexus.ponte:8081/repository/releases"
 };
